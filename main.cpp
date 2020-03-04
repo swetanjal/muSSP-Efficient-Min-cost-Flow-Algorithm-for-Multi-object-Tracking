ddc942659d973781608417096dd47d94eb21c432
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2020-03-04 23:12:12 +0530

    Add code to compute initial Shortest Path Tree.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "bits/stdc++.h"
 using namespace std;
+const long double INF = 1e18;
 const int MAXN = 1e5 + 5;
-int N, M; // Number of nodes, 
+int N, M; // Number of nodes, Number of edges
 vector <int> edges[MAXN];
 vector <long double> cost[MAXN];
+vector <long double> reduced_weight[MAXN];
+int indegree[MAXN];
 long double dist[MAXN];
 int shortest_path_tree_parent[MAXN];
+set < pair < int, int > > S;
 
+/*
+This function is responsible for reading in the graph.
+*/
 void initGraph(string filename)
 {
     char pr_type[3]; //problem type;
@@ -14,7 +21,6 @@ void initGraph(string filename)
     string line_inf;
     getline(file, line_inf);
     sscanf(line_inf.c_str(), "%*c %3s %d %d", pr_type, &N, &M);
-    
     for(string line; getline(file, line); )
     {
         switch(line[0]){
@@ -30,6 +36,7 @@ void initGraph(string filename)
                 sscanf(line.c_str(), "%*c %d %d %lf", &tail, &head, &weight);
                 edges[tail].push_back(head);
                 cost[tail].push_back(weight);
+                indegree[head]++;
                 break;
             }
             default:
@@ -38,9 +45,43 @@ void initGraph(string filename)
     }
 }
 
+// Computes Topological ordering of given graph and stores the order in vector topological_ordering(defined globally).
+vector <int> topological_ordering;
+void toposort(){
+    for(int i = 1; i <= N; ++i){
+        S.insert({indegree[i], i});
+    }
+    while(S.empty() == false){
+        int node = S.begin() -> second;
+        S.erase(S.begin());
+        for(int j = 0; j < edges[node].size(); ++j){
+            indegree[edges[node][j]]--;
+            S.erase(S.find({indegree[edges[node][j]] + 1, edges[node][j]}));
+            S.insert({indegree[edges[node][j]], edges[node][j]});
+        }
+        topological_ordering.push_back(node);
+    }
+}
+
+// Construct the initial Shortest Path tree
 void init_shortest_path_tree()
 {
-    
+    toposort();
+    int l = topological_ordering.size();
+    for(int i = 1; i <= N; ++i)
+        dist[i] = INF;
+    dist[topological_ordering[0]] = 0;
+    shortest_path_tree_parent[topological_ordering[0]] = -1; // Source
+    for(int i = 0; i < l; ++i){
+        int node = topological_ordering[i];
+        for(int j = 0; j < edges[node].size(); ++j){
+            long double updated_dist = dist[node] + cost[node][j];
+            if(updated_dist < dist[edges[node][j]]){
+                dist[edges[node][j]] = updated_dist;
+                shortest_path_tree_parent[edges[node][j]] = node;
+            }
+        }
+    }
 }
 
 int main(int argc, char * argv[])
